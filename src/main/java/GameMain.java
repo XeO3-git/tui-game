f48942e125d51103dbc0a68eeb2df9189e8c4e8d
xeo3-git <lgsky8@gmail.com>
2025-09-16 20:58:16 -0400

    fixed

--- src/main/java/GameMain.java
+++
@@ -1,5 +0,0 @@
-public class GameMain {
-    public static void main(String[] args) {
-        Level.getLevel().startMainLoop();
-    }  
-}
--- src/main/java/Movable.java
+++
@@ -1,3 +0,0 @@
-public abstract class Movable extends Drawable{
-    
-}
